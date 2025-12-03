70b5afa43bddaa4279c12094edf38aeab7e5585f
Huỳnh Phạm Nhật An <dh2594182@gmail.com>
2025-12-03 10:24:26 +0700

    Delete webbansach directory

--- webbansach/baitap/cau_12/README.md
+++
@@ -1,5 +0,0 @@
-
-La Thuấn Khang          110122090
-Ngô Huỳnh Quốc Khang    110122092
-Phan Đình Khải          110122089
-Nguyễn Thanh Duy        110122062
--- webbansach/ketnoi.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Kết nối đến máy chủ MySQL
-    $kn = mysqli_connect("localhost", "root", "") or die("Không thể kết nối: " . mysqli_error($kn));
-
-    // Chọn cơ sở dữ liệu
-    $csdl = mysqli_select_db($kn, "online_shop") or die("Không thể kết nối cơ sở dữ liệu: " . mysqli_error($kn));
-?>
